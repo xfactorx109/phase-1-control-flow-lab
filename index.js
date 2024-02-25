85f1820e959483cb467f3ca997b2107292c41e89
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2024-02-25 15:30:24 -0800

    passing tests

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance >= 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  } else {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+    switch (tip) {
+      case 'generous':
+        return "Thank you so much.";
+      case 'not as generous':
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
 }
